f66d41f951a8d60c6718898b3fd03eca9603d109
Chadro14 <altessemwamboko@gmail.com>
2025-05-30 15:18:03 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -59,5 +59,5 @@ async function playCommand(sock, chatId, message) {
 
 module.exports = playCommand; 
 
-/*Powered by KNIGHT-BOT*
-*Credits to Keith MD*`*/
+/*Powered by Alien-Assistance*
+*Credits to Son Altesse Royal*`*/
