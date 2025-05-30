4d090548184c2e3bff83bf421a7676a000e980ac
Chadro14 <altessemwamboko@gmail.com>
2025-05-30 16:08:55 +0100

    Update roseday.js

--- commands/roseday.js
+++ commands/roseday.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function rosedayCommand(sock, chatId) {
     try {
-        const shizokeys = 'knightbot';
+        const shizokeys = 'Alien Assistance';
         const res = await fetch(`https://api.shizo.top/quote/roseday?apikey=${shizokeys}`);
         
         if (!res.ok) {
