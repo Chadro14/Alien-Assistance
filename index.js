98036a59fddd1d6fddf35051113f2e3c8348153b
Chadro14 <altessemwamboko@gmail.com>
2025-05-31 11:42:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Alien Assistance - A WhatsApp Bot
+ * Knight Bot - A WhatsApp Bot
  * Copyright (c) 2024 Professor
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -155,7 +155,7 @@ async function startXeonBotInc() {
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
                                 newsletterJid: '120363161513685998@newsletter',
-                                newsletterName: 'Alien Assistance',
+                                newsletterName: 'KnightBot MD',
                                 serverMessageId: -1
                             }
                         }
@@ -253,8 +253,8 @@ async function startXeonBotInc() {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363161513685998@newsletter',
-                        newsletterName: 'Alien Assistance',
-                        assets/serverMessageId: -1
+                        newsletterName: 'KnightBot MD',
+                        serverMessageId: -1
                     }
                 }
             });
@@ -262,10 +262,10 @@ async function startXeonBotInc() {
             await delay(1999)
             console.log(chalk.yellow(`\n\n                  ${chalk.bold.blue(`[ ${global.botname || 'KNIGHT BOT'} ]`)}\n\n`))
             console.log(chalk.cyan(`< ================================================== >`))
-            console.log(chalk.magenta(`\n${global.themeemoji || '•'} YT CHANNEL: Alien Assistance `))
-            console.log(chalk.magenta(`${global.themeemoji || '•'} GITHUB: Chadro14`))
+            console.log(chalk.magenta(`\n${global.themeemoji || '•'} YT CHANNEL: MR UNIQUE HACKER`))
+            console.log(chalk.magenta(`${global.themeemoji || '•'} GITHUB: mrunqiuehacker`))
             console.log(chalk.magenta(`${global.themeemoji || '•'} WA NUMBER: ${owner}`))
-            console.log(chalk.magenta(`${global.themeemoji || '•'} CREDIT: Son Altesse royal `))
+            console.log(chalk.magenta(`${global.themeemoji || '•'} CREDIT: MR UNIQUE HACKER`))
             console.log(chalk.green(`${global.themeemoji || '•'} 🤖 Bot Connected Successfully! ✅`))
         }
         if (
